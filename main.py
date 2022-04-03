a18cf5de8b23a1f6b777c851ca800317cb2510e9
Cozoob <kozubmarcin10@gmail.com>
2022-04-03 18:20:20 +0200

    update venv

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import paho
+from kivy import App
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class TheLabApp(App):
+    pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    TheLabApp().run()
