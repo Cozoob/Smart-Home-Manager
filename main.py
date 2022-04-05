dd24763c80f1b9bd0cd8ef7cfa3c9854a364f426
rafibz007 <r.kaminski008@gmail.com>
2022-04-05 20:01:42 +0200

    maximizing window on start

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from GUI.main_layout import MainLayout
-
 from kivy.app import App
-from kivy import require
+from kivy.core.window import Window
 
+from kivy import require
 require('2.1.0')
 
 class MainApp(App):
     def build(self):
+        Window.size = (1280, 720)
+        Window.maximize()
         return MainLayout()
 
 
