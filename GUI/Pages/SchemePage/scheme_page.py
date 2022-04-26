85e63cf5a14ecb2881fc57875f204405c72953b0
rafibz007 <r.kaminski008@gmail.com>
2022-04-26 11:11:21 +0200

    rearregement

--- GUI/Pages/SchemePage/scheme_page.py
+++ GUI/Pages/SchemePage/scheme_page.py
@@ -30,8 +30,9 @@ import json
 
 # def get_max_floors_amount():
 #     return SchemePage._MAX_FLOORS_AMOUNT
+from GUI.Pages.SchemePage.scheme_object import SchemeObject, SchemeSensor
 
-Builder.load_file("GUI/Pages/scheme_page.kv")
+Builder.load_file("GUI/Pages/SchemePage/scheme_page.kv")
 
 
 class SchemePage(FloatLayout):
@@ -203,97 +204,6 @@ class SchemePage(FloatLayout):
         return self._MAX_FLOORS_AMOUNT
 
 
-class SchemeObject(BoxLayout):
-    """
-    Abstract class
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.selected = False
-        self.size_hint = [None, None]
-
-    # def on_touch_down(self, touch):
-    #     self.selected = True
-    #
-    # def on_touch_up(self, touch):
-    #     self.selected = False
-
-    @abstractmethod
-    def get_data(self):
-        pass
-
-    @abstractmethod
-    def change_position(self, pos):
-        pass
-
-    @abstractmethod
-    def select(self):
-        pass
-
-    @abstractmethod
-    def unselect(self):
-        pass
-
-
-class SchemeSensor(SchemeObject):
-    __SELECTED_COLOR = [1, 0, 1]
-    __DEFAULT_COLOR = [0, 1, 0]
-
-    counter = NumericProperty(0)
-    color = ListProperty(__DEFAULT_COLOR)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.size = [50, 50]
-        self.selected = False
-        with self.canvas:
-
-            self.ellipse = Ellipse(pos=self.pos, size=self.size)
-
-            self.line = Line(rectangle=(self.x, self.y, 0, 0), width=1,
-                             dash_length=5, dash_offset=2)
-
-        self.bind(pos=self._update_ellipse, size=self._update_ellipse)
-        self.bind(pos=self._update_line, size=self._update_line)
-
-    def _update_ellipse(self, *args):
-        self.ellipse.pos = self.pos
-        self.ellipse.size = self.size
-
-    def _update_line(self, *args):
-        if self.selected:
-            self.line.rectangle = (self.x - 5, self.y - 5,
-                                   self.width + 10, self.height + 10)
-        else:
-            self.line.rectangle = (self.x,self.y,0,0)
-
-    def get_data(self):
-        return {
-            "x": self.pos[0],
-            "y": self.pos[1],
-            "width": self.size[0],
-            "height": self.size[1],
-            "type": self.sensor.type,
-            "topic": self.sensor.topic
-        }
-
-    def change_position(self, pos):
-        self.pos = pos
-
-    def select(self):
-        self.selected = True
-        self._update_line()
-        self._update_ellipse()
-        self.color = self.__SELECTED_COLOR
-
-    def unselect(self):
-        self.selected = False
-        self._update_line()
-        self._update_ellipse()
-        self.color = self.__DEFAULT_COLOR
-
-
 class FloorCanvas(RelativeLayout):
     counter = NumericProperty(0)
     image_names = [img for img in listdir("./Resources/Images/")]
@@ -345,6 +255,7 @@ class FloorCanvas(RelativeLayout):
         else:
             # TODO
             # CHECK IF SENSOR CLICKED AND IF YES OPEN INFO POP UP WINDOW
+            print("Show sensor detail pop up window")
             pass
 
     def on_touch_move(self, touch):
